540a6ecb1a69da936908260a6f901009f1b4381f
Otumn <nersonnils@gmail.com>
2019-01-22 17:26:26 +0100

    Basic entity system

---
+++ Assets/Scripts/GameManagement/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Team17.BallDash
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameState state = new GameState();
+    }
+
+    public class GameState
+    {
+        private List<Entity> entities = new List<Entity>();
+
+        public void RegisterEntity(Entity ent)
+        {
+            entities.Add(ent);
+        }
+
+        public void UnregisterEntity(Entity ent)
+        {
+            entities.Remove(ent);
+        }
+
+        public void CallOnPlayerBeginTeleport()
+        {
+            for (int i = 0; i < entities.Count; i++)
+            {
+                entities[i].OnPlayerBeginTeleport();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Inheritance/Entity.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Team17.BallDash
+{
+    public class Entity : MonoBehaviour
+    { 
+        protected virtual void OnEnable()
+        {
+            GameManager.state.RegisterEntity(this);
+        }
+
+        protected void OnDisable()
+        {
+            GameManager.state.UnregisterEntity(this);
+        }
+
+        public virtual void OnPlayerBeginTeleport()
+        {
+
+        }
+    }
+}
