ce40ac79c4db2cc41fd57c7aedcaa6b4c45d190d
Otumn <nersonnils@gmail.com>
2019-01-22 16:47:04 +0100

    Ajout du projet initial, nous permettant de démarrer sainement, bisous !

---
+++ Assets/FPSCounter/FPSCounter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Otumn.FPSCounter
+{
+    public class FPSCounter : MonoBehaviour
+    {
+        [SerializeField] private Text fpsText;
+        float fps;
+
+        private void Start()
+        {
+            Application.targetFrameRate = 60;
+        }
+
+        private void Update()
+        {
+            fps = 1.0f / Time.deltaTime;
+            fpsText.text = fps.ToString("F") + " fps";
+        }
+    }
+}
