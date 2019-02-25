2bae1e304406150eb4b4adff2d0d80af7ef542a8
Otumn <nersonnils@gmail.com>
2019-02-25 18:29:16 +0100

    New ball control

--- Assets/Scripts/Inheritance/Entity/Character/Ball/PlayerProjectile.cs
+++ Assets/Scripts/Inheritance/Entity/Character/Ball/PlayerProjectile.cs
@@ -6,6 +6,7 @@ namespace Team17.BallDash
 {
     public class PlayerProjectile : Character
     {
+        enum TypeOfTimer {Cancel, Rehit, Nothing}
         [Header("Components")]
         [SerializeField] private Rigidbody body;
         [SerializeField] private TimersCalculator timer;
@@ -14,11 +15,12 @@ namespace Team17.BallDash
         [SerializeField] private Transform trajectory;
         [SerializeField] private Transform player;
         [Header("Parameters")]
+        [SerializeField] private TypeOfTimer type; 
         [SerializeField] private float speed = 50f;
         [SerializeField] private float slowedTimeScale = 0.2f;
         [SerializeField] private AnimationCurve speedMultiplier;
         [SerializeField] private AnimationCurve timeToHit;
-        private int numberOfHits = 0;
+        private float power = 0;
         private int reHitTimer;
         private bool wasCanceled = false;
         private Vector3 movementDirection;
@@ -46,7 +48,18 @@ namespace Team17.BallDash
         {
             Time.timeScale = slowedTimeScale;
             Time.fixedDeltaTime = 0.02f * Time.timeScale;
-            reHitTimer = timer.LaunchNewTimer(timeToHit.Evaluate(numberOfHits) * slowedTimeScale, CancelRehit);
+            switch(type)
+            {
+                case TypeOfTimer.Cancel:
+                    reHitTimer = timer.LaunchNewTimer(timeToHit.Evaluate(power) * slowedTimeScale, CancelRehit);
+                    break;
+                case TypeOfTimer.Rehit:
+                    reHitTimer = timer.LaunchNewTimer(timeToHit.Evaluate(power) * slowedTimeScale, CancelRehit);
+                    break;
+                case TypeOfTimer.Nothing:
+                    reHitTimer = timer.LaunchNewTimer(timeToHit.Evaluate(power) * slowedTimeScale, Nothing);
+                    break;
+            }
             timerFeedback.gameObject.SetActive(true);
             trajectory.gameObject.SetActive(true);
             player.gameObject.SetActive(true);
@@ -56,7 +69,7 @@ namespace Team17.BallDash
         public void FeedBack(Vector3 touchPos)
         {
             Timer t = timer.GetTimerFromUserIndex(reHitTimer);
-            timerFeedback.localScale = Mathf.InverseLerp(0, t.MaxTime, t.TimeLeft) * initialFeedbackScale;
+            timerFeedback.localScale = (Mathf.InverseLerp(0, t.MaxTime, t.TimeLeft) * initialFeedbackScale) + Vector3.one;
             trajectory.position = Vector3.Lerp(transform.position, touchPos, 0.5f);
             float zRot = Vector3.SignedAngle(transform.up, (touchPos - transform.position), Vector3.forward);
             trajectory.rotation = Quaternion.Euler(0, 0, zRot);
@@ -75,21 +88,25 @@ namespace Team17.BallDash
             body.useGravity = false;
             Time.timeScale = 1;
             Time.fixedDeltaTime = 0.02f * Time.timeScale;
-            movementDirection = newDirection.normalized * (speed * speedMultiplier.Evaluate(numberOfHits));
+            Timer t = timer.GetTimerFromUserIndex(reHitTimer);
+            power += (t.Inc / slowedTimeScale) * 1.2f;
+            Debug.Log("Power after shot : " + power + ", Added : " + (t.Inc / slowedTimeScale) * 1.2f);
+            movementDirection = newDirection.normalized * (speed * speedMultiplier.Evaluate(power));
             body.velocity = movementDirection;
             timer.DeleteTimer(reHitTimer);
-            numberOfHits++;
             timerFeedback.gameObject.SetActive(false);
             trajectory.gameObject.SetActive(false);
             player.gameObject.SetActive(false);
         }
 
+        #region Type of timer
+
         private void CancelRehit()
         {
             Time.timeScale = 1;
             Time.fixedDeltaTime = 0.02f * Time.timeScale;
-            numberOfHits = 0;
-            body.velocity = body.velocity * 0.5f;
+            power = 0;
+            body.velocity = body.velocity * 0.7f;
             body.useGravity = true;
             wasCanceled = true;
             timerFeedback.gameObject.SetActive(false);
@@ -97,13 +114,27 @@ namespace Team17.BallDash
             player.gameObject.SetActive(false);
         }
 
+        private void Nothing()
+        {
+            Time.timeScale = 1;
+            Time.fixedDeltaTime = 0.02f * Time.timeScale;
+            timerFeedback.gameObject.SetActive(false);
+            trajectory.gameObject.SetActive(false);
+            player.gameObject.SetActive(false);
+        }
+
+        #endregion
+
         private void Bounce(Vector3 enterVector, Vector3 collisionNormal)
         {
+            if (body.useGravity) return;
             Vector3 newDir = Vector3.Reflect(enterVector, collisionNormal);
             lastNewDir = newDir;
-            movementDirection = newDir.normalized * (speed * speedMultiplier.Evaluate(numberOfHits));
+            power -= 5;
+            if (power < 0) power = 0;
+            Debug.Log("Power after bounce : " + power);
+            movementDirection = newDir.normalized * (speed * speedMultiplier.Evaluate(power));
             body.velocity = movementDirection;
-            Debug.Log("New dir : " + newDir);
         }
 
         private void OnTriggerEnter(Collider coll)
