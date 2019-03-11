8eb316757562211512762673a0dfe494a93a6761
Unknown <Robin.seignolles@hotmail.fr>
2019-03-11 02:07:28 +0100

    Début prototypage boss Serpent

---
+++ Assets/Scripts/GameManagement/FollowPath.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class FollowPath : MonoBehaviour
+{
+    [SerializeField] private PathCreator pathCreator;
+    [SerializeField] private BossManagement bossManager;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private bool startMoveAlongSpline; 
+    private float distanceTravelled;
+    private bool launchNext;
+    private int pathCount;
+
+    public void Update()
+    {
+        Debug.Log(pathCreator.path.ended);
+        if(Input.GetMouseButtonDown(1))
+        {
+            pathCreator.path.ended = false;
+        }
+
+        if(pathCreator.path.ended == false)
+        {
+            launchNext = false;
+            distanceTravelled += speed * Time.fixedDeltaTime;
+            transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, EndOfPathInstruction.Stop);
+            transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, EndOfPathInstruction.Stop);
+        }
+        else if(pathCreator.path.ended == true && launchNext)
+        {
+            pathCount++;
+            launchNext = false;
+            Debug.Log("Chemin numéro" + pathCount + "effectué");
+        }
+
+    }
+}
---
+++ Assets/Scripts/Inheritance/Entity/Character/Enemies/Boss/Snake/SnakeBoss.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Team17.BallDash
+{
+    public class SnakeBoss : Boss
+    {
+        #region NotNow
+        /*
+                [Header("Snake parameters")]
+                [SerializeField] private BossAimZone zone;
+                [SerializeField] private SnakeBossPattern[] patterns;
+                [SerializeField] private BossAimZone[] zones;
+
+                protected override void Update()
+                {
+                    base.Update();
+                }
+
+                [ContextMenu("Add")]
+                public void Add()
+                {
+                    zones = new BossAimZone[1];
+                    zones[0] = new BossAimZone();
+                }
+            */
+        #endregion
+        [SerializeField] private SnakeBossPattern[] patternsArray;
+
+
+    }
+
+    [System.Serializable]
+    public struct SnakeBossPattern
+    {
+        [SerializeField] private string name;
+        [SerializeField] private FollowPath splineArray;
+        //[SerializeField] private BossAimZone zone;
+    }
+
+
+
+
+
+}
