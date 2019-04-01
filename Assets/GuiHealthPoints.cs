46b68d26124378dbc5d65712f7788fbbbc7ae6cf
Unknown <tdauplay97@gmail.com>
2019-04-01 18:07:41 +0200

    Ui in game, Pause Menu
    Gui des HP et début du pause menu

---
+++ Assets/GuiHealthPoints.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Team17.BallDash
+{
+    public class GuiHealthPoints : MonoBehaviour
+    {
+        public GameObject[] healthPointsArray;
+        [Range(0, 3)]
+        public int PlayerHpTempActual = 3; //variable temporaire des HP du joueur (à remplacer par la vrai variable).
+        int LastPlayerHp = 0;
+
+        private void Start()
+        {
+            LastPlayerHp = PlayerHpTempActual;
+        }
+
+        void Update()
+        {
+            if (PlayerHpTempActual < LastPlayerHp)
+            {
+                LastPlayerHp = PlayerHpTempActual;
+                healthPointsArray[PlayerHpTempActual].SetActive(false);
+            }
+        }
+    }
+}
