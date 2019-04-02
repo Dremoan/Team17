8289e1ed4aa3a3126663696a944180fc792ba534
Otumn <nersonnils@gmail.com>
2019-04-02 20:46:00 +0200

    small things

---
+++ Assets/Scripts/Inheritance/Entity/Character/Enemies/Boss/BossWeakPoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Team17.BallDash
+{
+    public class BossWeakPoint : Character, IBallHitable
+    {
+        [SerializeField] private Boss linkedBoss;
+
+        public void Hit(float dmgs)
+        {
+            linkedBoss.Hit(dmgs);
+        }
+    }
+}
