8dadd6f5b81ad7819ab16a9c1e67cc672abf4e78
Otumn <nersonnils@gmail.com>
2019-04-02 19:17:12 +0200

    game instructions

---
+++ Assets/Scripts/Inheritance/Entity/UiElement/BeginingInstructions.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace Team17.BallDash
+{
+    public class BeginingInstructions : UiElement
+    {
+        [SerializeField] private GameObject beginGameText;
+        [SerializeField] private GameObject controlsText;
+
+        protected override void Start()
+        {
+            base.Start();
+            beginGameText.SetActive(true);
+        }
+
+        public override void OnIntroLaunched()
+        {
+            base.OnIntroLaunched();
+            beginGameText.SetActive(false);
+        }
+
+        public override void OnBossBeginsPatterns()
+        {
+            base.OnBossBeginsPatterns();
+            controlsText.SetActive(true);
+        }
+
+        public override void OnPlayerTeleport()
+        {
+            base.OnPlayerTeleport();
+            controlsText.SetActive(false);
+        }
+    }
+}
