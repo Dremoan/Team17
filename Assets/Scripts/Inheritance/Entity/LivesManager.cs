e2fa6b64e5ae15a4c5b4de2aaf54c39cf397de7e
Unknown <Robin.seignolles@hotmail.fr>
2019-04-04 10:06:20 +0200

    Tir Set up

--- Assets/Scripts/Inheritance/Entity/LivesManager.cs
+++ Assets/Scripts/Inheritance/Entity/LivesManager.cs
@@ -10,6 +10,7 @@ namespace Team17.BallDash
         [SerializeField] private PlayerProjectile secondBall;
         [SerializeField] private PlayerProjectile thirdBall;
         [SerializeField] private PlayerProjectile fourthBall;
+        [SerializeField] private PlayerCharacter playerCharacter;
         private int livesLeft = 4;
 
         protected override void Start()
@@ -32,21 +33,25 @@ namespace Team17.BallDash
             if (livesLeft == 4)
             {
                 firstBall.gameObject.SetActive(true);
+                playerCharacter.actualBall = firstBall.GetComponent<PlayerProjectile>();
                 return firstBall;
             }
             if (livesLeft == 3)
             {
                 secondBall.gameObject.SetActive(true);
+                playerCharacter.actualBall = secondBall.GetComponent<PlayerProjectile>();
                 return secondBall;
             }
             if (livesLeft == 2)
             {
                 thirdBall.gameObject.SetActive(true);
+                playerCharacter.actualBall = thirdBall.GetComponent<PlayerProjectile>();
                 return thirdBall;
             }
             if (livesLeft == 1)
             {
                 fourthBall.gameObject.SetActive(true);
+                playerCharacter.actualBall = fourthBall.GetComponent<PlayerProjectile>();
                 return fourthBall;
             }
             return null;
