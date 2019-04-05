15b4919dd293bfce4879ff5dad627ceb119dde6a
Otumn <nersonnils@gmail.com>
2019-04-05 10:18:37 +0200

    Snake tail prefab

---
+++ Assets/Scripts/Inheritance/Entity/Character/Enemies/Boss/Snake/WarpSpawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Team17.BallDash
+{
+    public class SnakeWarpSpawner : Entity
+    {
+        [SerializeField] private FeedBack warpFB;
+
+
+    }
+}
