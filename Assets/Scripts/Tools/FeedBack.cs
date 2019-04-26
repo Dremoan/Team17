f61f22b6057faa44e82051c0a82ec41c1d8f5964
Otumn <nersonnils@gmail.com>
2019-04-26 16:46:06 +0200

    Zoom feedback base

--- Assets/Scripts/Tools/FeedBack.cs
+++ Assets/Scripts/Tools/FeedBack.cs
@@ -25,20 +25,25 @@ namespace Team17.StreetHunt
         [SerializeField] private float shakeAmplitude = 0.2f;
         [SerializeField] private float shakeTime = 0.1f;
         //zoom
+        [Tooltip("The zoom will be applied on the position of this gameobject, therefore it's often better to use this option with 'tpOnTranformOnPlay' or 'hardFollowingTranform'.")]
         [SerializeField] private bool zoom = false;
         [SerializeField] private AnimationCurve zoomInCurve;
         [SerializeField] private AnimationCurve zoomOutCurve;
-        [SerializeField] private float speed = 0.1f;
+        [SerializeField] private float zoomedDist = 4f;
+        [SerializeField] private float zoomSpeed = 0.1f;
         //rumble
         [SerializeField] private bool rumble = false;
         [SerializeField] private long rumbleTime = 5;
 
 
-        private Transform[] usedTransform;
+        private Transform[] usedShakeTransform;
+        private Vector3[] initialZoomTargetsPos;
         private bool isShaking = false;
         private bool isRumbling = false;
-        private bool isZooming = false;
+        private bool isZoomingIn = false;
+        private bool isZoomingOut = false;
         private float shakeDecrementer;
+        private float zoomIncrementer;
 
         private void Start()
         {
@@ -49,6 +54,7 @@ namespace Team17.StreetHunt
         private void Update()
         {
             ShakeManagement();
+            ZoomManagement();
             PositionManagement();
         }
 
@@ -80,18 +86,28 @@ namespace Team17.StreetHunt
             {
                 if(useSpecificTransform)
                 {
-                    usedTransform = specificTransformToShake;
+                    usedShakeTransform = specificTransformToShake;
                 }
                 else
                 {
-                    usedTransform = new Transform[GameManager.state.VirtualCameraShakeTargets.Count];
-                    for (int i = 0; i < usedTransform.Length; i++)
+                    usedShakeTransform = new Transform[GameManager.state.VirtualCameraShakeTargets.Count];
+                    for (int i = 0; i < usedShakeTransform.Length; i++)
                     {
-                        usedTransform[i] = GameManager.state.VirtualCameraShakeTargets[i].transform;
+                        usedShakeTransform[i] = GameManager.state.VirtualCameraShakeTargets[i].transform;
                     }
                 }
                 shakeDecrementer = shakeTime;
                 isShaking = true;
+            }
+
+            if(zoom)
+            {
+                initialZoomTargetsPos = new Vector3[GameManager.state.VirtualCameraZoomTargets.Count];
+                for (int i = 0; i < initialZoomTargetsPos.Length; i++)
+                {
+                    initialZoomTargetsPos[i] = GameManager.state.VirtualCameraZoomTargets[i].transform.position;
+                }
+                isZoomingIn = true;
             }
 
             if(rumble)
@@ -131,6 +147,11 @@ namespace Team17.StreetHunt
                     shakeDecrementer = shakeTime;
                     isShaking = false;
                 }
+                if(zoom)
+                {
+                    isZoomingOut = true;
+                    isZoomingIn = false;
+                }
                 if(rumble)
                 {
                     isRumbling = false;
@@ -145,16 +166,68 @@ namespace Team17.StreetHunt
                 if(shakeDecrementer > 0)
                 {
                     if(!looping) shakeDecrementer -= Time.deltaTime; 
-                    for (int i = 0; i < usedTransform.Length; i++)
+                    for (int i = 0; i < usedShakeTransform.Length; i++)
                     {
                         Vector3 newPos = (Random.insideUnitCircle * shakeAmplitude * Mathf.InverseLerp(0, shakeTime, shakeDecrementer));
-                        usedTransform[i].localPosition = new Vector3(newPos.x, newPos.y, usedTransform[i].localPosition.z);
+                        usedShakeTransform[i].localPosition = new Vector3(newPos.x, newPos.y, usedShakeTransform[i].localPosition.z);
                     }
                 }
                 else
                 {
                     shakeDecrementer = shakeTime;
                     isShaking = false;
+                }
+            }
+        }
+
+        private void ZoomManagement()
+        {
+            if(isZoomingIn)
+            {
+                if(zoomIncrementer < 1f)
+                {
+                    zoomIncrementer += Time.deltaTime * zoomSpeed;
+                    float newZ = Mathf.Lerp(0, zoomedDist, zoomInCurve.Evaluate(zoomIncrementer));
+                    for (int i = 0; i < GameManager.state.VirtualCameraZoomTargets.Count; i++)
+                    {
+                        float newX = Mathf.Lerp(initialZoomTargetsPos[i].x, transform.position.x, zoomInCurve.Evaluate(zoomIncrementer));
+                        float newY = Mathf.Lerp(initialZoomTargetsPos[i].y, transform.position.y, zoomInCurve.Evaluate(zoomIncrementer));
+                        GameManager.state.VirtualCameraZoomTargets[i].transform.position = new Vector3(newX, newY, newZ);
+                    }
+                }
+                else
+                {
+                    if(!looping)
+                    {
+                        zoomIncrementer = 0f;
+                        isZoomingOut = true;
+                        isZoomingIn = false;
+                    }
+                    else
+                    {
+                        isZoomingIn = false;
+                        zoomIncrementer = 0f;
+                    }
+                }
+            }
+            else if(isZoomingOut)
+            {
+                if (zoomIncrementer < 1)
+                {
+                    zoomIncrementer += Time.deltaTime * zoomSpeed;
+                    float newZ = Mathf.Lerp(0, zoomedDist, zoomOutCurve.Evaluate(zoomIncrementer));
+                    for (int i = 0; i < GameManager.state.VirtualCameraZoomTargets.Count; i++)
+                    {
+                        float newX = Mathf.Lerp(initialZoomTargetsPos[i].x, transform.position.x, zoomOutCurve.Evaluate(zoomIncrementer));
+                        float newY = Mathf.Lerp(initialZoomTargetsPos[i].y, transform.position.y, zoomOutCurve.Evaluate(zoomIncrementer));
+                        GameManager.state.VirtualCameraZoomTargets[i].transform.position = new Vector3(newX, newY, newZ);
+                    }
+                }
+                else
+                {
+                    zoomIncrementer = 0f;
+                    isZoomingOut = false;
+                    isZoomingIn = false;
                 }
             }
         }
@@ -184,5 +257,6 @@ namespace Team17.StreetHunt
         public ParticleSystem[] ParticlesSystems { get => particlesSystems; set => particlesSystems = value; }
         public TrailRenderer[] TrailRenderers { get => trailRenderers; set => trailRenderers = value; }
         public bool Rumble { get => rumble; }
+        public bool Zoom { get => zoom; }
     }
 }
