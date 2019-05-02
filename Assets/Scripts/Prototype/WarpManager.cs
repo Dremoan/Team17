0c5d681c32ce495b76f311fd42a6a9000b5fa899
Unknown <Robin.seignolles@hotmail.fr>
2019-05-02 11:07:12 +0200

    Je push ça

--- Assets/Scripts/Prototype/WarpManager.cs
+++ Assets/Scripts/Prototype/WarpManager.cs
@@ -16,11 +16,12 @@ namespace Team17.StreetHunt
 
         IEnumerator WarpsAppear()
         {
+            yield return new WaitForSeconds(.5f);
             for (int i = 0; i < warps.Length; i++)
             {
-                yield return new WaitForSeconds(.5f);
                 warps[i].SetActive(true);
-                yield return new WaitForSeconds(0.1f);
+                yield return null;
+                //yield return new WaitForSeconds(0.1f);
             }
             snakeBossScript.PickMove();
         }
