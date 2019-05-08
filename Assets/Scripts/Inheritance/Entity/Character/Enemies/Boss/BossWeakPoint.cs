7b56abca26291053e5c5b1d387ac1b11a5beb3f7
Unknown <Robin.seignolles@hotmail.fr>
2019-05-08 11:38:39 +0200

    WeakPoint Tweaked

--- Assets/Scripts/Inheritance/Entity/Character/Enemies/Boss/BossWeakPoint.cs
+++ Assets/Scripts/Inheritance/Entity/Character/Enemies/Boss/BossWeakPoint.cs
@@ -9,10 +9,15 @@ namespace Team17.StreetHunt
         [SerializeField] private Boss linkedBoss;
         [SerializeField] private GameObject actualTouchPlane;
         [SerializeField] private FeedBack deathFeedback;
+        [SerializeField] private Material healthBarMat;
+        [SerializeField] private Texture newTexture;
+        [SerializeField] private float blinkTime = 0.1f;
+        private bool alreadyDead;
 
         public void Hit(int index, float dmgs)
         {
             linkedBoss.Hit(index, dmgs);
+            StartCoroutine(Blink());
         }
 
         public override void OnBossDeath()
@@ -20,6 +25,19 @@ namespace Team17.StreetHunt
             base.OnBossDeath();
             actualTouchPlane.SetActive(false);
             deathFeedback.Play();
+            healthBarMat.SetTexture("_Tex", newTexture);
+        }
+
+        IEnumerator Blink()
+        {
+            if (healthBarMat != null) healthBarMat.SetFloat("_Threshold", 1f);
+            yield return new WaitForSeconds(.1f);
+            if (healthBarMat != null) healthBarMat.SetFloat("_Threshold", 0f);
+            yield return new WaitForSeconds(.1f);
+            if (healthBarMat != null) healthBarMat.SetFloat("_Threshold", 1f);
+            yield return new WaitForSeconds(.1f);
+            if (healthBarMat != null) healthBarMat.SetFloat("_Threshold", 0f);
         }
     }
+
 }
