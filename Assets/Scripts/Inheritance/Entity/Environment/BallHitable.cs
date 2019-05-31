066f472eca691e023b310cc5917d00dd4050d098
Otumn <nersonnils@gmail.com>
2019-05-31 14:44:20 +0200

    Bug fix

---
+++ Assets/Scripts/Inheritance/Entity/Environment/BallHitable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Team17.StreetHunt
+{
+    public class BallHitable : Environment, IBallHitable
+    {
+        void IBallHitable.Hit(int index, float dmgs)
+        {
+            
+        }
+    }
+}
