139366f806f6f4cefe86bcc723be7685842fab22
Otumn <nersonnils@gmail.com>
2019-05-31 15:53:40 +0200

    Pause script

---
+++ Assets/Scripts/Tools/PauseScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+    [SerializeField] private KeyCode pauseKey = KeyCode.Space;
+
+    private bool paused = false;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(pauseKey))
+        {
+            Debug.Break();
+        }
+    }
+}
