6415f2b770676de064c4adda2a3cd35c65b28e5a
Unknown <Robin.seignolles@hotmail.fr>
2019-06-01 17:58:42 +0200

    Tutorial

---
+++ Assets/Scripts/Inheritance/Entity/TouchPlaneTutorial.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Team17.StreetHunt
+{
+    public class TouchPlaneTutorial : Entity, Touchable
+    {
+        [SerializeField] private UnityEvent eventTouch;
+
+        public void OnTouchBegin(Vector3 touchPos)
+        {
+            
+        }
+
+        public void OnTouchHeld(Vector3 touchPos)
+        {
+            
+        }
+
+        public void OnTouchReleased(Vector3 touchPos)
+        {
+            eventTouch.Invoke();
+        }
+    }
+}
