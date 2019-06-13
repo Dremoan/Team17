155140ad89ff75939290662cc77b3d90081ad758
Otumn <nersonnils@gmail.com>
2019-06-13 15:07:18 +0200

    Gorilla weak point positions

---
+++ Assets/Scripts/Tools/LocalRotationLocker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalRotationLocker : MonoBehaviour
+{
+    private Quaternion initialRotation;
+
+    private void Awake()
+    {
+        initialRotation = transform.rotation;
+    }
+
+    private void Update()
+    {
+        transform.rotation = initialRotation;
+    }
+}
